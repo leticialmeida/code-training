c47d6b0e7f8e9972b1778ff040fa27a06a7b233d
Letícia Rodrigues Almeida Ribeiro <Leticia@Inovas-MacBook-Pro.local>
2017-12-13 12:07:36 -0200

    Começamos jquery e o form que nao foi na outra aula

---
+++ code_aula2/static/JS/script.js
@@ -0,0 +1,14 @@
+
+    $("#image1").click(
+        function (){
+            $(this).addClass("background-red");
+        },
+        function (){
+            $(this).removeClass("background-red");
+        }
+    ) ;
+  
+
+
+//onde.quando = o que
+
